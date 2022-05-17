2c21972cf5367850dcce578d969d9c3c4fa9e3bf
patagonia <phong280201@gmail.com>
2022-05-17 14:02:21 +0700

    update file

--- src/index.js
+++ src/index.js
@@ -5,24 +5,32 @@ const { engine } = require('express-handlebars');
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(morgan('combined'))
     //template engine
 app.engine('hbs', engine({
     extname: '.hbs'
 }));
-app.set('view engine', 'hbs');
+app.set('view engine', "hbs");
 app.set('views', path.join(__dirname, 'resources\\views'));
-
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
 app.get('/', (req, res) => {
-    res
-    // res.setHeader('Content-type', 'application/json')
-    // res.setHeader('X-Powered-By', 'Node.js')
     res.render('home');
 })
 app.get('/news', (req, res) => {
-    res
-    // res.setHeader('Content-type', 'application/json')
-    // res.setHeader('X-Powered-By', 'Node.js')
+
     res.render('news');
 })
+app.get('/search', (req, res) => {
+    res.render('search')
+})
+app.post('/search', (req, res) => {
+    console.log(req.body)
+    res.send('')
+})
+
 app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}`))
